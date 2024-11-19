087e7c783b8352e3b9161cadcc5ff0d83f2e6c69
Brolin <brolinblaze@gmail.com>
2024-11-19 11:46:00 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3989563526"
+    serviceWorkerVersion: "2065865338"
   }
 });
